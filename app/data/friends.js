0f8e178332863edaad45c4bf9bc089a590ed8f44
Grayson M <grayson.mcmurry23@gmail.com>
2019-12-05 15:31:20 -0600

    added starter files

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+{
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
